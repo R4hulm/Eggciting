d117081dea691556085a3b1cdcd4bfab94f015a4
unknown <rahulmurugesh.work@gmail.com>
2022-04-13 01:28:24 +0530

    added cartIcon and header cart Button components

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,13 +1,13 @@
-// import React from 'react';
 import { Fragment } from "react";
+import HeaderCartButton from "./HeaderCartButton";
 import classes from "./Header.module.css";
 
 const Header = (props) => {
     return (
         <Fragment>
             <header className={classes.header}>
-                <h1>Eggciting</h1>
-                <button>Cart</button>
+                <h1 className={classes.name}>Eggciting</h1>  
+                <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>
                 <img src = "https://images.pexels.com/photos/4397266/pexels-photo-4397266.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"

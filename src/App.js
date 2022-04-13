3c1252c7aa40e1232cd453092579465c1c79c803
unknown <rahulmurugesh.work@gmail.com>
2022-04-14 01:06:16 +0530

    added MealItem, AvailableMeals, Meals and Card components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { Fragment } from "react";
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
-    <div className="App"> 
+    <Fragment>
     <Header />
-    </div>
-
+    <main>
+      <Meals />
+    </main>
+    </Fragment>
   );
 }
 

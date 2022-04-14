45354db63ecea40e5904d847880dbc417965f4e5
unknown <rahulmurugesh.work@gmail.com>
2022-04-15 02:07:06 +0530

    added Cart, Meals and Modal components

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,4 +1,5 @@
 import Card from '../UserInterface/card';
+import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
@@ -29,7 +30,14 @@ const DUMMY_MEALS = [
   ];
 
 const AvailableMeals =() => {
-    const mealsList = DUMMY_MEALS.map(meal=><li>{meal.name}</li>);
+    const mealsList = DUMMY_MEALS.map(meal=>(
+    <MealItem
+        id={meal.id}
+        key = {meal.id}
+        name = {meal.name}
+        description = {meal.description}
+        price = {meal.price}
+    />));
 return <section className={classes.meals}>
     <Card>
         <ul>
---
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,21 @@
+import Input from '../../UserInterface/input';
+import classes from './MealItemForm.module.css';
+
+const MealItemForm = (props) => {
+    return (
+        <form className={classes.form}>
+            <Input label = 'Amount' input = {{
+                id:'amount_',
+                type:'number',
+                min:'1',
+                max:'5',
+                step:'1',
+                defaultValue: '1',
+                }} 
+            />
+            <button>+Add</button>
+        </form>
+    )
+}
+
+export default MealItemForm;

783ee1ff6021831c2d71999d21c469d037aeff69
unknown <rahulmurugesh.work@gmail.com>
2022-04-16 23:56:33 +0530

    added remove functionality to cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -3,7 +3,7 @@ import CartContext from "./CartContext";
 
 const defaultCartState = {
     items: [],
-    totalAmount: 0
+    totalAmount: 0,
 };
 
 const cartReducer = (state, action) => {
@@ -26,12 +26,34 @@ const cartReducer = (state, action) => {
         } else {
             updatedItems = state.items.concat(action.item);
         }
-        
+
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
         };
     }
+
+    if(action.type==='REMOVE'){
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.id);
+        const existingCartItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+        let updatedItems;
+        if(existingCartItem.amount ===1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+
+        }else {
+            const updatedItem = {...existingCartItem, amount: existingCartItem.amount -1};
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        };
+
+        return ({
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        });
+    }
+    
     return defaultCartState;
 };
 
